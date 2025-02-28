de4c9f34cf2462f1957b7326baa463dd313c7d0c
Sbongumusa2003 <u22710362@tuks.co.za>
2025-02-28 06:17:53 +0200

    Final

--- SbongumusaStudyGroup/Models/Group27.cs
+++ SbongumusaStudyGroup/Models/Group27.cs
@@ -16,7 +16,6 @@ namespace SbongumusaStudyGroup.Models
 		[DisplayName("SURNAME")]
 		public string Surname { get; set; }
 		[DisplayName("EMAIL ADDRESS")]
-		[Required(ErrorMessage = "Student's Email is Required")]
 		public string Email { get; set; }
 	}
 }
