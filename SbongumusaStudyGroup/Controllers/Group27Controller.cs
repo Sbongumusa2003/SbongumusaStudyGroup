32808c4b8f002b5dca75104f475e8f0f3c6903ab
Sbongumusa Mbatha <u22710362@tuks.co.za>
2025-02-28 05:19:46 +0200

    Add project files.

---
+++ SbongumusaStudyGroup/Controllers/Group27Controller.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SbongumusaStudyGroup.Models;
+
+namespace SbongumusaStudyGroup.Controllers
+{
+    public class Group27Controller : Controller
+    {
+        // GET: Group27
+        public ActionResult Index()
+        {
+            List<Group27> Students = new List<Group27>
+            {
+                new Group27
+                {
+                    StudentNumber = "u01234567",
+                    Name ="Jane",
+                    Surname = "Doe",
+                    Email = "jane.doe@tuks.co.za",
+                },
+                new Group27
+                {
+                    StudentNumber = "u12345678",
+                    Name ="John",
+                    Surname = "Doe",
+                    Email = "john.doe@tuks.co.za",
+                },
+                new Group27
+                {
+                    StudentNumber = "u5237841",
+                    Name ="Lindo",
+                    Surname = "Nkosi",
+                    Email = "lindo.nkosi@tuks.co.za",
+                },
+                new Group27
+                {
+                    StudentNumber = "u6785243",
+                    Name ="Pearl",
+                    Surname = "Mbatha",
+                    Email = "pearl.mbatha@tuks.co.za",
+                },
+                new Group27
+                {
+                    StudentNumber = "u5972327",
+                    Name ="Lungile",
+                    Surname = "Masina",
+                    Email = "lungile.masina@tuks.co.za",
+                }
+            };
+            return View(Students);
+        }
+    }
+}
+;
---
+++ SbongumusaStudyGroup/Models/Group27.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SbongumusaStudyGroup.Models
+{
+	public class Group27
+	{
+		[DisplayName("STUDENT NUMBER")]
+		public string StudentNumber { get; set; }
+		[DisplayName("NAME")]
+		public string Name { get; set; }
+		[DisplayName("SURNAME")]
+		public string Surname { get; set; }
+		[DisplayName("EMAIL ADDRESS")]
+		public string Email { get; set; }
+	}
+}
